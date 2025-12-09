d3f1f83421ec11d808fbbf07bfd1f5450a35ca62
archx64 <neucleyon@gmail.com>
2025-12-09 14:02:10 +0700

    remove deprecated scripts

--- src/deprecated/calibration/take_photo_cam1.py
+++
@@ -1,6 +0,0 @@
-from open_cctv import capture_and_save_photo
-
-# replace with CCTV camera's URL e.g. url = 'rtsp://192.168.1.41:554/mcast/11'
-url = 'rtsp://admin:csimAIT5706@192.168.6.101:554/Streaming/Channels/101/' 
-
-capture_and_save_photo(url=url, save_dir='cam1', filename_prefix='cam1', ext='png', delay=1, window_name='Camera 1', image_number=13)
